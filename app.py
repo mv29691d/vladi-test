722f87badb0d8ebd2c2630ddbb11edfbb314bfb5
mv29691d <133343390+mv29691d@users.noreply.github.com>
2023-05-12 11:07:27 +0200

    Create app.py

---
+++ app.py
@@ -0,0 +1,14 @@
+import os
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def main():
+    return "Welcome to Docker MMCOMS!"
+
+@app.route('/howareyou')
+def hello():
+    return 'I am good, how about you?'
+
+if __name__ == "__main__":
+    app.run()
